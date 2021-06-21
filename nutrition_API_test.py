d3ea462a1fca2dba8878a1dca73c7477908d6c5b
EveRuby <evgeniyarubin@gmail.com>
2021-06-21 14:08:35 -0400

    Update nutrition_API_test.py

--- nutrition_API_test.py
+++ nutrition_API_test.py
@@ -14,5 +14,5 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 print(response.text)
 print(type(response.text))
 
-#{"recipesUsed":18,"calories":{"value":530.0,"unit":"calories","confidenceRange95Percent":{"min":446.62,"max":604.76},"standardDeviation":171.15},"fat":{"value":39.0,"unit":"g","confidenceRange95Percent":{"min":29.08,"max":44.13},"standardDeviation":16.28},"protein":{"value":22.0,"unit":"g","confidenceRange95Percent":{"min":19.42,"max":26.16},"standardDeviation":7.29},"carbs":{"value":28.0,"unit":"g","confidenceRange95Percent":{"min":20.85,"max":31.07},"standardDeviation":11.06}}
+#{"recipesUsed":18,"calories":
 
