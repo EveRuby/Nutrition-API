9e782e7ef0abe1ce86ccf608875272b47823a10d
nsh240 <74507655+nsh240@users.noreply.github.com>
2021-06-24 20:48:10 -0400

    madded catch for bad recipe entry

--- app/nutrition_API_Main.py
+++ app/nutrition_API_Main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pprint import pprint
+import sys
 
 API_KEY = os.environ["API_KEY"]
 
@@ -37,8 +38,6 @@ def meal_plan(targetCalories_input, diet_input, exclude_input):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     parsed_response = json.loads(response.text)
-    print(parsed_response)
-    print(parsed_response["meals"])
     return parsed_response
 
 
@@ -76,17 +75,21 @@ if __name__ == '__main__':
 
         # response = requests.request("GET", url, headers=headers, params=querystring)
         # parsed_response = json.loads(response.text)
-        calories=int(parsed_response["calories"]["value"])
-        fat=int(parsed_response["fat"]["value"])
-        carbs=int(parsed_response["carbs"]["value"])
-        protein=int(parsed_response["protein"]["value"])
-        print("------------------------")
-        print("NUTRITION INFORMATION FOR", recipe+":" )
-        print("------------------------")
-        print("CALORIES:", calories)
-        print("PROTEIN: ", protein, "grams")
-        print("FAT: ", fat, "grams")
-        print("CARBOHYDRATES: ", carbs, "grams")
+        if "calories" not in parsed_response.keys():
+            print("****PLEASE ENTER A VALID RECIPE!!****")
+            sys.exit()
+        else:
+            calories=int(parsed_response["calories"]["value"])
+            fat=int(parsed_response["fat"]["value"])
+            carbs=int(parsed_response["carbs"]["value"])
+            protein=int(parsed_response["protein"]["value"])
+            print("------------------------")
+            print("NUTRITION INFORMATION FOR", recipe+":" )
+            print("------------------------")
+            print("CALORIES:", calories)
+            print("PROTEIN: ", protein, "grams")
+            print("FAT: ", fat, "grams")
+            print("CARBOHYDRATES: ", carbs, "grams")
         
 
     elif task.upper() == "B":
@@ -142,22 +145,14 @@ if __name__ == '__main__':
         print("LUNCH:", lunch)
         print("DINNER:", dinner)
         print("------------------------")
-        print("TOTAL DAILY NUTRIOTION:")
+        print("TOTAL DAILY NUTRITION:")
         print("------------------------")
         print("CALORIES:", total_cal)
         print("PROTEIN:", total_prot)
         print("FAT:", total_fat)
         print("CARBOHYDRATES:", total_carb)
         
-        #sending an email
-        #email_request=input("WOULD YOU LIKE AN EMAIL WITH THE ABOVE INFORMATION - Y/N?")
-        #if email_request == "Y":
-        #    print("------------------------")
-        #    email=input("PLEASE ENTER YOUR EMIAL ADDRESS ")
-        #    print("GREAT! PLEASE ALLOW FOR A FEW MINUTES FOR THE EMAIL TO SHOW UP IN YOUR INBOX")
 
-    #else:
-        #print("Please re-run the app and enter a valid value")
 
     print("------------------------")
     print ("THANK YOU FOR USING MEAL PALL!")
